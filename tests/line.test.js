ac15f4261fb42421083038c30cccab685079b030
Alex Marinov <alex@diylab.uk>
2024-10-19 09:33:51 +0100

    renaming test folder to tests

--- tests/line.test.js
+++ tests/line.test.js
@@ -1,42 +1,13 @@
 import fs from "fs";
-import { log } from "console";
 import { IGESLoader } from "../src/IGESLoader";
 import * as THREE from "three";
 
 describe("IGESLoader", () => {
-  it("loader should be type of IGESLoader", () => {
-    const loader = new IGESLoader();
-    expect(loader).toBeInstanceOf(IGESLoader);
-  });
-
-  it("should parse a point", () => {
-    const loader = new IGESLoader();
-    expect(loader).toBeInstanceOf(IGESLoader);
-
-    const filePath = "test/models/point.iges";
-    const data = fs.readFileSync(filePath, "utf8");
-
-    let test = loader.parse(data);
-    let points = test.children[0];
-    expect(points).toBeInstanceOf(THREE.Points);
-
-    let pointBufferGeometry = points.geometry;
-    expect(pointBufferGeometry).toBeInstanceOf(THREE.BufferGeometry);
-
-    let pointVertice = pointBufferGeometry;
-    let pointVertice_x = pointVertice.attributes.position.array[0];
-    let pointVertice_y = pointVertice.attributes.position.array[1];
-    let pointVertice_z = pointVertice.attributes.position.array[2];
-    expect(pointVertice_x).toBe(10);
-    expect(pointVertice_y).toBe(20);
-    expect(pointVertice_z).toBe(30);
-  });
-
   it("should parse a line", () => {
     const loader = new IGESLoader();
     expect(loader).toBeInstanceOf(IGESLoader);
 
-    const filePath = "test/models/line.iges";
+    const filePath = "tests/models/line.iges";
     const data = fs.readFileSync(filePath, "utf8");
 
     let test = loader.parse(data);
